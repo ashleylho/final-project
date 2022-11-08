ec4cac450e2a480e1982f1973cd5fb66c740ece4
Ashley Ho <ashley.lho@gmail.com>
2022-11-08 17:47:49 +0000

    inserted values into profileTypes and edgeTech table

--- database/data.sql
+++ database/data.sql
@@ -5,3 +5,12 @@ insert into "shapes" ("shapeName", "shapeDescription")
 values ('True Twin', 'A true twin snowboard shape is ideal for riders who want to ride switch. The nose and tail are identical and the flex distributes throughout the entirety of the board. True twins are symmetrical.'),
        ('Directional', 'Setback stance with a longer nose that easily floats above powder and the stiff tail maintains stability at high speeds. This shape is ideal for freeride and powder conditions.'),
        ('Asymmetrical Twin', 'Balance through asymmetry. This shape features asymmetrical sidecuts and contact lengths. It has a deeper sidecut on the heel side, as well as longer toe side contact and shorter heel side contact for finesse and power.'),
+
+insert into "profileTypes" ("profileName", "profileDescription")
+values ('Original Banana', 'Original Banana Technology is easy and awesome thanks to mild rocker between your feet combined with mild cambers to the tips and tails.'),
+       ('C2', 'Perfectly balanced all terrain contour. Mild rocker between your feet with powerful cambers to the tips and tails. Enough power pop and precision to technically hit the biggest wedges, enough float and freedom to rip the gnarliest AK pow lines. Carves the resort, plays in the park, and slays powder.'),
+       ('C2x', 'A shorter, more aggressive banana rocker combined with cambers that sit more underfoot. Extra solid tip and tail pressure for power, pop, precision and end-to-end stability combined with a medium amount of pressure between your feet for carving, edge hold on ice and float in pow.'),
+       ('C3', 'Aggressive dominant camber zones out to the contacts combined with mild rocker between your feet. Subtle Banana rocker gives freestyle freedom with maximum end-to-end stability for big, high-speed lines. For aggressive, skilled riders.')
+
+insert into "edgeTech" ("edgeTechName", "edgeTechDescription")
+values ('Magne-Traction®', '7 strategically located and sized edge serrations provide unreal edge hold and control in all conditions.')
