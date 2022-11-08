cab8d3430bbab49ce4e007eece2b2ac710b9599e
Ashley Ho <ashley.lho@gmail.com>
2022-11-08 02:29:53 +0000

    inserted values for shapeName and shapeDescription into shapes table #1

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,7 @@
 -- insert into "snowboards" ("name", "price", "imageUrl", "description", "profileType", "flex", "shape", "edgeTech", "abilityLevel", "terrain")
 -- values ('Roxy XOXO', ')
+
+insert into "shapes" ("shapeName", "shapeDescription")
+values ('True Twin', 'A true twin snowboard shape is ideal for riders who want to ride switch. The nose and tail are identical and the flex distributes throughout the entirety of the board. True twins are symmetrical.'),
+       ('Directional', 'Setback stance with a longer nose that easily floats above powder and the stiff tail maintains stability at high speeds. This shape is ideal for freeride and powder conditions.'),
+       ('Asymmetrical Twin', 'Balance through asymmetry. This shape features asymmetrical sidecuts and contact lengths. It has a deeper sidecut on the heel side, as well as longer toe side contact and shorter heel side contact for finesse and power.'),
