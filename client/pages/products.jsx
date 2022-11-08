d36084a3b2b2872a68e2eaae7458e8a60377c9a0
Ashley Ho <ashley.lho@gmail.com>
2022-11-08 22:51:21 +0000

    created a fetch request for all snowboards and parsed the response #1

--- client/pages/products.jsx
+++ client/pages/products.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 export default class Products extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(products => this.setState({ products }))
+      .catch(err => console.error(err));
+  }
 
   render() {
     return (
