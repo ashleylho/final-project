e6577e969c2d490813524550e050e1fe50e14651
Ashley Ho <ashley.lho@gmail.com>
2022-11-11 18:06:25 +0000

    added cart icon to navbar #3

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -10,7 +10,10 @@ export default class Navigation extends React.Component {
           <div className='overlay'>
             <Nav className="align-items-center">
               <Navbar.Brand className="brand mb-0" href="#home">{'Trader Snow\'s'}</Navbar.Brand>
-              <Nav.Link href="#products">PRODUCTS</Nav.Link>
+              <div className="d-flex">
+                <Nav.Link href="#products" className="">PRODUCTS</Nav.Link>
+                <Nav.Link className="px-0 mt-0 fs-2" href="#cart"><i className="bi bi-cart-fill" /></Nav.Link>
+              </div>
             </Nav>
           </div>
         </div>
